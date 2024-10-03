534bb2b0a0d3d0d38ba2156c6312250a5ea18879
AlisaTerzieva <alisa.terzieva@gmail.com>
2024-10-03 11:00:00 +0300

    Transferred the fetch calls into a dedicated client file. The file will be the single point, responsible for handling API calls and storing API endpoints. Created an infinite scroll for fetching the top most rated movies. Each request fetches the next 20 records from the server, utilizing the pagination, introduced in the backend. Removed styling from the JSX in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,59 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { MovieCard } from './MovieCard.jsx';
+import { getMoviesWithSearchPhrase, getTopRatedMoviesWeek } from './api/the-movie-db-client.js';
 
-const baseMovieUrl = 'https://api.themoviedb.org/3/';
-const searchMovieEndpoint = 'search/movie?query=';
-const apiToken = import.meta.env.VITE_THE_MOVIE_DB_API_TOKEN;
 
 function App() {
   const [searchPhrase, setSearchPhrase] = useState('');
   const [movieData, setMovieData] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getTopRatedMoviesWeek(pageNumber)
+      .then(data => {
+        setMovieData([...movieData, ...data]);
+        setIsLoading(false);
+      });
+  }, [pageNumber]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handlePageScroll);
+
+    return () => window.removeEventListener('scroll', handlePageScroll);
+  });
 
   const handleSearchChange = (event) => {
     setSearchPhrase(event.target.value);
   }
 
   const handleSearchClick = () => {
-    fetch(baseMovieUrl + searchMovieEndpoint + searchPhrase, {
-      headers: {
-        Authorization: 'Bearer ' + apiToken
-      }
-    })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error("HTTP Search request failed with status code: ", response.status)
-    })
-    .then(data => {
-      if (!data) {
-        throw new Error("Movie Data not found!");
-      }
-      const parsedData = data.results.map(res => ({id: res.id, title: res.title, overview: res.overview, poster: res.poster_path, releaseDate: res.release_date}));
-      setMovieData(parsedData);
-    })
-    .catch(error => console.log(error));
+    getMoviesWithSearchPhrase(searchPhrase)
+      .then(data => {
+      // TODO: Include data checks
+      setMovieData(data);
+      });
+  }
+
+  const handlePageScroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 500 && !isLoading) {
+      setPageNumber(num => num + 1);
+      setIsLoading(true);
+    }
   }
 
   return (
     <>
       <h1>Welcome to the Movie Search App!</h1>
       <input className='search-input' type='text' name='search' value={searchPhrase} onChange={handleSearchChange}/>
-      <button style={{marginLeft: '10px'}} onClick={handleSearchClick}>Search!</button>
+      <button className='search-btn' onClick={handleSearchClick}>Search!</button>
       <div className='movie-cards'>
         {movieData && movieData.map(movie => <MovieCard key={movie.id} movie={movie}/>)}
       </div>
+      {isLoading && <div>LOADING... {pageNumber}</div>}
     </>
   )
 }
 
-export default App
+export default App;
